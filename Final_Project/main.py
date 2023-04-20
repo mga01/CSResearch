d328b359fd87a89943f1ffe518a14a601a86eafe
VillhemVonka <43239509+VillhemVonka@users.noreply.github.com>
2023-04-20 00:06:03 -0400

    Updates to main.py

--- Final_Project/main.py
+++ Final_Project/main.py
@@ -1,25 +1,38 @@
 import cv2
 import mediapipe as mp
 import math
+import tkinter
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
-# This uses video capture 1, Windows usually uses 0
+root = tkinter.Tk()
+
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+root.destroy()
+
+# initialize video capture object to read video from external webcam
 cap = cv2.VideoCapture(1)
+# if there is no external camera then take the built-in camera
+if not cap.read()[0]:
+    cap = cv2.VideoCapture(0)
 
 tileSize = 150
 selectorSize = 20
 
-players = [{'x': 0, 'y': 0, 'symbol': 'X', 'old_index_distance': 0, 'index_distance': 0, 'is_clicking': False},
-           {'x': 0, 'y': 0, 'symbol': 'O', 'old_index_distance': 0, 'index_distance': 0, 'is_clicking': False}]
+players = [{'x': 0, 'y': 0, 'symbol': 'X', 'old_index_distance': 0, 'index_distance': 0, 'is_clicking': False, 'color': (0, 255, 0)},
+           {'x': 0, 'y': 0, 'symbol': 'O', 'old_index_distance': 0, 'index_distance': 0, 'is_clicking': False, 'color': (0, 0, 255)}]
 
 grid = [
   ['', '', ''],
   ['', '', ''],
   ['', '', ''],
 ]
+
+def rectangle(x, y, w, h, color, fill):
+  cv2.rectangle(image, (x, y), (x + w, y + h), (color), fill)
 
 def detectMousePosition(event, x, y, flags, params):
    if event == cv2.EVENT_LBUTTONDBLCLK:
@@ -44,18 +57,21 @@ def drawGrid(image):
             playerY <= squareY + tileSize and\
             playerY + selectorSize >= squareY:
             if player['is_clicking']:
-              color = (0, 0, 255)
+              color = (75, 75, 75)
+              grid[y][x] = player['symbol']
               player['is_clicking'] = False
             break
           
         cv2.rectangle(image, (squareX, squareY), (squareX + tileSize, squareY + tileSize), color, -1)
         cv2.rectangle(image, (squareX, squareY), (squareX + tileSize, squareY + tileSize), (0, 0, 0), 2)
-          
+
+        cv2.putText(image, grid[y][x], (squareX + 50, squareY + int(tileSize / 1.5)), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), 2, cv2.LINE_AA)
 
 with mp_hands.Hands(
     model_complexity=0,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands:
+    min_detection_confidence=0.8,
+    min_tracking_confidence=0.5,
+    ) as hands:
   while cap.isOpened():
     success, image = cap.read()
     if not success:
@@ -66,11 +82,17 @@ with mp_hands.Hands(
     results = hands.process(image)
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-    cv2.rectangle(image, (0, 0), (image.shape[1], image.shape[0]), (100, 100, 100), -1)
+    cv2.rectangle(image, (0, 0), (screen_width, screen_height), (100, 100, 100), -1)
 
     index = 0
 
     drawGrid(image)
+    
+    # removes the outer lines of boxes on the edges of the board
+    rectangle(int(image.shape[1] / 3) + (tileSize * 3) - 2, int(image.shape[0] / 5) - 1, 3, tileSize * 3 + 2, (100, 100, 100), -1)
+    rectangle(int(image.shape[1] / 3) - 2, int(image.shape[0] / 5) - 1, 3, tileSize * 3 + 2, (100, 100, 100), -1)
+    rectangle(int(image.shape[1] / 3) - 2, int(image.shape[0] / 5) - 1, tileSize * 3 + 2, 3, (100, 100, 100), -1)
+    rectangle(int(image.shape[1] / 3) - 2, int(image.shape[0] / 5) + (tileSize * 3) - 1, tileSize * 3 + 2, 3, (100, 100, 100), -1)
 
     # check distance between points 9 and 6 (index finger) (if has decreased)
 
@@ -78,10 +100,10 @@ with mp_hands.Hands(
       for hand_landmarks in results.multi_hand_landmarks:
         keypoints = []
 
-        shape_color = (255, 0, 0)
+        shape_color = players[index]['color']
 
         if index >= 2:
-          index = 1
+          break
 
         for data_point in hand_landmarks.landmark:
           keypoints.append({'x': data_point.x, 'y': data_point.y})
@@ -94,15 +116,16 @@ with mp_hands.Hands(
 
         if players[index]['index_distance'] + 0.02 < players[index]['old_index_distance']:
           players[index]['is_clicking'] = True
-
+        
         cv2.rectangle(image, (players[index]['x'], players[index]['y']), (players[index]['x'] + selectorSize, players[index]['y'] + selectorSize), shape_color, -1)
         cv2.rectangle(image, (players[index]['x'], players[index]['y']), (players[index]['x'] + selectorSize, players[index]['y'] + selectorSize), (0, 0, 0), 2)
         
-      
         index += 1
-
+    
+    image = cv2.resize(image, (screen_width, screen_height))
     cv2.imshow('Tic-Tac-Toe', cv2.flip(image, 1))
-    cv2.setMouseCallback('Tic-Tac-Toe',detectMousePosition)
+    cv2.setMouseCallback('Tic-Tac-Toe', detectMousePosition)  
+
     if cv2.waitKey(1) == ord('q'):
         break
     
